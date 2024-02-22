142953310120327e00f2e7ceaf8019dfd8ea1466
95-preeti <preetiyadav30430@gmail.com>
2024-02-22 19:24:47 +0530

    button addded

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 import  Greet from  './component/Greet'
 import Welcome from './component/Welcome'
 import Hello from './component/Hello'
+import Message from './component/Message';
+import Counter from './component/Counter'
 function App (){
   return (
     <div className="App">
-      <Greet />
-      <Greet/>
-      <Greet/>
-      {/*<Welcome  />*/}
+      < Counter />
+     {/*} <Message />
+      {/*<Greet  name="bruce" heroName="batman">
+      <p>This is children props</p></Greet>
+      <Greet name="clark" heroName="superman">
+        <button>Action</button>
+      </Greet>
+      <Greet name="diana" heroName="wonder woman"/>
+      <Welcome name="bruce" heroName="batman" />
+      <Welcome  name="clark" heroName="superman" />
+      <Welcome name="diana" heroName="wonder woman" />
       {/*<Hello />*/}
       
     </div>
--- src/component/Greet.js
+++ src/component/Greet.js
@@ -4,8 +4,18 @@ import React from 'react';
     //return <h1>hello preeti </h1>
 //}
 
-   const Greet =() => <h1>Hello mom</h1>
+   const Greet =(props) =>{
+   console.log(props)
+   return(
+    <div>
+        <h1>
+            Hello  {props.name} a.k.a {props.heroName}
+            </h1>
+            {props.children}
+    </div>
+   
 
-
+   )
+   }
 
    export default Greet
